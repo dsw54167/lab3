d6ee665ee878062da306fc71901c86690284448b
michal.mlynarczyk <m.mlynarczyk@benefitsystems.pl>
2024-04-27 12:12:16 +0200

    first part

---
+++ commands.sh
@@ -0,0 +1,44 @@
+intro
+/1 commit
+git commit
+git commit
+/2 branching
+git branch bugFix
+git checkout bugFix
+/3 merge
+git checkout -b bugFix
+git commit
+git checkout -
+git commit
+git merge bugCix
+/4 rebasing
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout -
+git rebase main
+
+ramping up
+
+/5 detaching
+git checkout C4
+/6
+git checkout bugFix
+git checkout HEAD^
+/7
+git checkout HEAD^
+git branch -f main C6
+git branch -f bugFix HEAD^
+/8
+git reset HEAD^
+git checkout pushed
+git revert C2
+
+moving around
+/9
+git cherry pick C3 C4 C7
+/10
+git rebase -i HEAD~4
+/mixed bag
+
